39dc00ad864ab08f7b439bc5741d3232a856998b
dnosur <45685967+dnosur@users.noreply.github.com>
2024-12-04 17:37:30 +0200

    feat(TilesetPropertiesController): add tileset properties controller class

---
+++ Dangeon&Dragons/Dodge/tiny/TilesetProperties/TilesetPropertiesController.cpp
@@ -0,0 +1,86 @@
+#include "TilesetPropertiesController.h"
+#include "PropertiesFactory.h"
+
+TilesetPropertiesController::TilesetPropertiesController()
+{
+}
+
+TilesetPropertiesController::TilesetPropertiesController(std::vector<Propertie> tilesetProperties)
+	: tilesetProperties(tilesetProperties)
+{
+	this->tilesetProperties = tilesetProperties;
+}
+
+TilesetPropertiesController::TilesetPropertiesController(tinyxml2::XMLElement* tileset)
+{
+	readTilesetProperties(tileset, tilesetProperties);
+}
+
+TilesetPropertiesController::~TilesetPropertiesController()
+{
+}
+
+std::vector<Propertie>::iterator TilesetPropertiesController::begin()
+{
+	return tilesetProperties.begin();
+}
+
+std::vector<Propertie>::iterator TilesetPropertiesController::end()
+{
+	return tilesetProperties.end();
+}
+
+int TilesetPropertiesController::GetSize()
+{
+	return tilesetProperties.size();
+}
+
+Propertie* TilesetPropertiesController::operator[](const char* name)
+{
+	for (Propertie& propertie : tilesetProperties) {
+		if (!strcmp(propertie.getTitle(), name)) {
+			return &propertie;
+		}
+	}
+	return nullptr;
+}
+
+Propertie* TilesetPropertiesController::operator[](int index)
+{
+	return nullptr;
+}
+
+void TilesetPropertiesController::readTilesetProperties(tinyxml2::XMLElement* tileset, std::vector<Propertie>& tilesetProperties)
+{
+	tinyxml2::XMLElement* properties = getTilesetProperties(tileset);
+
+	for (
+		tinyxml2::XMLElement* child = properties->FirstChildElement("property");
+		child != nullptr;
+		child = child->NextSiblingElement("property")
+		) {
+		tilesetProperties.push_back(
+			PropertiesFactory::createPropertie(
+				(char*)child->Attribute("name"),
+				(char*)child->Attribute("value"),
+				(char*)child->Attribute("type")
+			)
+		);
+	}
+}
+
+tinyxml2::XMLElement* TilesetPropertiesController::getTilesetProperties(tinyxml2::XMLElement* tileset)
+{
+	for (
+		tinyxml2::XMLElement* child = tileset->FirstChildElement();
+		child != nullptr;
+		child = child->NextSiblingElement()
+		) {
+		if (strcmp(child->Attribute("type"), "TilesetProperties")) {
+			break;
+		}
+
+		return child->FirstChildElement("properties");
+	}
+	return nullptr;
+}
---
+++ Dangeon&Dragons/Dodge/tiny/TilesetProperties/TilesetPropertiesController.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <tinyxml2.h>
+#include <vector>
+
+#include "Propertie.h"
+
+class TilesetPropertiesController
+{
+	std::vector<Propertie> tilesetProperties;
+public:
+	TilesetPropertiesController();
+	TilesetPropertiesController(std::vector<Propertie> tilesetProperties);
+	TilesetPropertiesController(tinyxml2::XMLElement* tileset);
+	~TilesetPropertiesController();
+
+	std::vector<Propertie>::iterator begin();
+	std::vector<Propertie>::iterator end();
+
+	int GetSize();
+
+	Propertie* operator[](const char* name);
+	Propertie* operator[](int index);
+
+	static void readTilesetProperties(tinyxml2::XMLElement* tileset, std::vector<Propertie>& tilesetProperties);
+	static tinyxml2::XMLElement* getTilesetProperties(tinyxml2::XMLElement* tileset);
+};
