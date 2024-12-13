872a2f272bd8ca8663aa17cef27212c94d7a3ec7
dnosur <45685967+dnosur@users.noreply.github.com>
2024-12-13 17:32:49 +0200

    feat(Movement): add movement structure

---
+++ Dangeon&Dragons/Content/Scripts/AI/Movement.cpp
@@ -0,0 +1,24 @@
+#include "Movement.h"
+
+Movement::Movement()
+{
+    complete = false;
+    title = nullptr;
+
+	action = Actions::Idle;
+	direction = Directions::DOWN;
+	animation = nullptr;
+}
+
+Movement::Movement(
+    const char* title, Directions direction, Actions action, 
+    IAnimation* animation, Coord position)
+{
+	copyStr(title, this->title);
+	complete = false;
+
+	this->direction = direction;
+	this->action = action;
+	this->animation = animation;
+	this->position = position;
+}
---
+++ Dangeon&Dragons/Content/Scripts/AI/Movement.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "../../../Dodge/Directions.h"
+#include "../../../Dodge/animator/IAnimation.h"
+#include "../../../Dodge/pawn/Actions.h"
+
+struct Movement
+{
+	char* title;
+
+	bool complete;
+
+	Directions direction;
+	Actions action;
+
+	IAnimation* animation;
+
+	Coord position;
+
+	Movement();
+	Movement(
+		const char* title, Directions direction, Actions action, 
+		IAnimation* animation, Coord position
+	);
+}; 
