83f917bc2ed72cbda83494c3b04d9cce39362ef7
dnosur <45685967+dnosur@users.noreply.github.com>
2024-12-13 17:50:10 +0200

    fix(TinyClass): fix bugs

--- Dangeon&Dragons/Dodge/tiny/TinyClass.cpp
+++ Dangeon&Dragons/Dodge/tiny/TinyClass.cpp
@@ -28,43 +28,49 @@ void TinyClass::GetObjects(tinyxml2::XMLElement* element, std::vector<ICollision
 		return;
 	}
 
-	for (tinyxml2::XMLElement* object = objectGroup->FirstChildElement("object");
-		object != nullptr;
-		object = object->NextSiblingElement("object"))
-	{
-		const Coord coord = Coord(object->DoubleAttribute("x"), object->DoubleAttribute("y"));
-		const Size size = Size(object->DoubleAttribute("width"), object->DoubleAttribute("height"));
+	for (tinyxml2::XMLElement* objectGroup = element->FirstChildElement("objectgroup");
+		objectGroup != nullptr;
+		objectGroup = objectGroup->NextSiblingElement("objectgroup")
+		) {
 
-		const int object_id = object->IntAttribute("id");
-		const char* object_name = object->Attribute("name");
-		const char* object_type = object->Attribute("type");
-
-		tinyxml2::XMLElement* polygon = object->FirstChildElement("polygon");
-		if (polygon == nullptr)
+		for (tinyxml2::XMLElement* object = objectGroup->FirstChildElement("object");
+			object != nullptr;
+			object = object->NextSiblingElement("object"))
 		{
+			const Coord coord = Coord(object->DoubleAttribute("x"), object->DoubleAttribute("y"));
+			const Size size = Size(object->DoubleAttribute("width"), object->DoubleAttribute("height"));
+
+			const int object_id = object->IntAttribute("id");
+			const char* object_name = object->Attribute("name");
+			const char* object_type = object->Attribute("type");
+
+			tinyxml2::XMLElement* polygon = object->FirstChildElement("polygon");
+			if (polygon == nullptr)
+			{
+				objects.push_back(
+					new BoxCollision(
+						coord,
+						size,
+						object_id,
+						(char*)object_name,
+						(char*)object_type
+					)
+				);
+				continue;
+			}
+
 			objects.push_back(
-				new BoxCollision(
-					coord,
-					size,
+				new PoligonCollision(
+					TinyXml::ParsePolygon(
+						polygon->Attribute("points"),
+						coord
+					),
 					object_id,
 					(char*)object_name,
 					(char*)object_type
 				)
 			);
-			continue;
 		}
-
-		objects.push_back(
-			new PoligonCollision(
-				TinyXml::ParsePolygon(
-					polygon->Attribute("points"),
-					coord
-				),
-				object_id,
-				(char*)object_name,
-				(char*)object_type
-			)
-		);
 	}
 }
 
