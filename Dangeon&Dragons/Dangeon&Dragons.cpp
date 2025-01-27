b9bf919fe8b226bc7028ba9cc454ed4f94921076
dnosur <45685967+dnosur@users.noreply.github.com>
2025-01-27 11:23:53 +0200

    feat(Dangeon&Dragons): add memory leak debug

--- Dangeon&Dragons/Dangeon&Dragons.cpp
+++ Dangeon&Dragons/Dangeon&Dragons.cpp
@@ -1,7 +1,17 @@
-// Dangeon&Dragons.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// debug_new.cpp
+// compile by using: cl /EHsc /W4 /D_DEBUG /MDd debug_new.cpp
+#define _CRTDBG_MAP_ALLOC
+#include <cstdlib>
+#include <crtdbg.h>
 
-//#define _CRTDBG_MAP_ALLOC
+#ifdef _DEBUG
+#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+// Replace _NORMAL_BLOCK with _CLIENT_BLOCK if you want the
+// allocations to be of _CLIENT_BLOCK type
+#else
+#define DBG_NEW new
+#endif
+
 #define STB_IMAGE_IMPLEMENTATION
 #define GL_GLEXT_PROTOTYPES
 
@@ -14,9 +24,6 @@
 #include "Dodge/audio/SoundSystem.h"
 
 #include "Dodge/tiny/TinyXml.h"
-
-#include <cstdlib>
-#include <crtdbg.h>
 
 int main(int argc, char** argv)
 {
@@ -34,6 +41,7 @@ int main(int argc, char** argv)
     main.Initialize();
     main.Update();
 
+    _CrtDumpMemoryLeaks();
     return 0;
 }
 
