e6104c2e9fb8697d2f6a4a09ecda0d489c0fdbb2
dnosur <45685967+dnosur@users.noreply.github.com>
2025-01-30 11:30:55 +0200

    change(Node): change shared_ptr to unique_ptr

--- Dangeon&Dragons/Content/Scripts/AI/Node.h
+++ Dangeon&Dragons/Content/Scripts/AI/Node.h
@@ -8,17 +8,17 @@
 
 // Структура узла для A*
 struct Node {
-    std::shared_ptr<Movement> movement;
+    std::unique_ptr<Movement> movement;
     float gCost; // Стоимость пути от начальной точки
     float hCost; // Эвристическая стоимость до цели
     float fCost() const { return gCost + hCost; } // Общая стоимость
     std::shared_ptr<Node> parent;
 
-    Node(std::shared_ptr<Movement> movement,
+    Node(std::unique_ptr<Movement> movement,
         float gCost, float hCost, 
         std::shared_ptr<Node> parent = nullptr
     )
-        : movement(movement), gCost(gCost), hCost(hCost), parent(parent) {}
+        : movement(std::move(movement)), gCost(gCost), hCost(hCost), parent(parent) {}
 
     //~Node() {
     //    delete parent;
