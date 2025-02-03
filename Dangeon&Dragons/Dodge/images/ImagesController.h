5cc80064b9da7fc8fa2ae7a49fd7063fb896e55b
dnosur <45685967+dnosur@users.noreply.github.com>
2025-02-03 12:03:04 +0200

    feat(ImagesController): using unordered maps

--- Dangeon&Dragons/Dodge/images/ImagesController.h
+++ Dangeon&Dragons/Dodge/images/ImagesController.h
@@ -2,7 +2,10 @@
 #define GL_GLEXT_PROTOTYPES
 #include <glad/glad.h>
 #include <GL/freeglut.h>
+
 #include <vector>
+#include <unordered_map>
+#include <filesystem>
 
 #include "../Size.h"
 #include "../Coord.h"
@@ -11,28 +14,32 @@
 #include "../lib/stb_image.h"
 #include "../Color.h"
 
+namespace fs = std::filesystem;
+
 class ImagesController
 {
-	std::vector<Image> images;
+	std::unordered_map<std::string, Image> images;
 
 	static std::shared_ptr<Image> defaultImage;
 
-	int GetIndexByTitle(std::string& title);
+	int GetIndexByTitle(std::string_view title);
 	void ChangeIfExist(Image image);
 
 	void Draw(Image& item, Coord& position, Color& color, Size& windowSize, Size& size, bool reverse = false);
 public:
-	static Image LoadImg(std::string path, std::string title);
+	static Image LoadImg(std::string_view path, std::string title);
 
 	static void SetDefaultImage(std::unique_ptr<Image> image);
 	static std::weak_ptr<Image> GetDafaultImage();
 
-	void Load(std::string path, std::string title, Shader* shader = nullptr);
-	void LoadAndDrawImage(std::string path, std::string title, Shader* shader, Coord position, Size size, Size windowSize);
+	void LoadFromFolder(std::string_view path);
 
-	void DrawImage(std::string title, Coord position, Size size, Size windowSize, Color color = Color(1.0f, 1.0f, 1.0f), bool reverse = false);
+	void Load(std::string_view path, std::string title, Shader* shader = nullptr);
+	void LoadAndDrawImage(std::string_view path, std::string title, Shader* shader, Coord position, Size size, Size windowSize);
 
-	std::vector<Image> GetImages();
+	void DrawImage(std::string_view title, Coord position, Size size, Size windowSize, Color color = Color(1.0f, 1.0f, 1.0f), bool reverse = false);
+
+	const std::unordered_map<std::string, Image>& GetImages();
 	void SetImages(std::vector<Image> images);
 
 	int GetSize();
@@ -40,5 +47,5 @@ public:
 	void Clear();
 
 	Image* operator[](int index);
-	Image* operator[](std::string title);
+	Image* operator[](std::string_view title);
 };
