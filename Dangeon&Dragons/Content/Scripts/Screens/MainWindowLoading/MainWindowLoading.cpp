717e3aa0f41500a38f1735b2e1f21de51e9defdf
dnosur <45685967+dnosur@users.noreply.github.com>
2025-02-07 18:50:33 +0200

    feat(MainWindowLoading): add own random

--- Dangeon&Dragons/Content/Scripts/Screens/MainWindowLoading/MainWindowLoading.cpp
+++ Dangeon&Dragons/Content/Scripts/Screens/MainWindowLoading/MainWindowLoading.cpp
@@ -1,8 +1,12 @@
 #include "MainWindowLoading.h"
+
+#include <memory>
 #include <filesystem>
 
 #include"../../../../Dodge/utilities/string.h"
 #include "../../UI/ProgressBar/TextureProgressBar/TextureProgressBar.h"
+#include "../../../../Dodge/utilities/random.h"
+#include "../../../../Dodge/font/Fonts.h"
 
 void MainWindowLoading::Initialize()
 {
@@ -22,21 +26,39 @@ void MainWindowLoading::Initialize()
 				return;
 			}
 
-			*imageIndexWeak.lock() = rand() % imagesWeak.lock()->GetSize();
+			*imageIndexWeak.lock() = randm(0, imagesWeak.lock()->GetSize() - 1);
 	});
 }
 
 void MainWindowLoading::Draw()
 {
+	const Size& windowSize = Window::GetRenderResolutionView();
+	if (images->GetSize()) {
+		images->DrawImage(images->operator[](*imageIndex)->title, Coord(0, 0), windowSize, windowSize);
+		Fonts::GetFont("DreiFraktur")->RenderText(
+			L"Подсказка касаемо того как вам играть.. Что-то там ещё туда сюда какой-то текст со смыслом!",
+			Coord(Window::GetSize().width / 4.0f, 150),
+			std::make_unique<FontRenderOptions>(
+				1.0f,
+				.0f,
+				std::make_unique<Size>(1000, 90),
+				std::make_unique<Padding>(0, 15.0f, 0, 0),
+				Color(0.93f, 0.89f, 0.41f)
+			)
+		);
+	}
 }
 	
-MainWindowLoading::MainWindowLoading(int maxValue) 
+MainWindowLoading::MainWindowLoading(int maxValue)
 	: LoadingScreen(
 		std::make_unique<TextureProgressBar>(
 			Coord(Window::GetRenderResolution().GetWidth() / 4, 0),
 			Window::GetRenderResolutionView() / 2,
 			"Content/UI/LoadBar/",
-			maxValue
+			maxValue,
+			0,
+			false,
+			std::make_unique<Color>(1, 1, 1)
 		)
 	)
 {
@@ -50,9 +72,6 @@ std::weak_ptr<ProgressBar> MainWindowLoading::GetProgressBar()
 
 void MainWindowLoading::Update()
 {
-	const Size& windowSize = Window::GetRenderResolutionView();
-	if (images->GetSize()) {
-		images->DrawImage(images->operator[](*imageIndex)->title, Coord(0, 0), windowSize, windowSize);
-	}
+	Draw();
 	progressBar->Update();
 }
