12fd9c2753d8519a6478e62e04ede161f2a0ebe2
dnosur <45685967+dnosur@users.noreply.github.com>
2025-02-20 12:57:40 +0200

    feat(RenderInstance): create virtual render class

--- Dangeon&Dragons/Dodge/render/RenderInstance.cpp
+++ Dangeon&Dragons/Dodge/render/RenderInstance.cpp
@@ -1,1 +1,88 @@
 #include "RenderInstance.h"
+#include <glad/glad.h>
+
+void RenderInstance::UpdateVertices()
+{
+    GenerateVertices();
+    if (!VBO || !VAO || !EBO) {
+        return Initialize();
+    };
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    void* ptr = glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
+    if (ptr) {
+        memcpy(ptr, vertices.data(), vertices.size() * sizeof(float));
+        glUnmapBuffer(GL_ARRAY_BUFFER);
+    }
+}
+
+void RenderInstance::UpdateVertices(std::vector<float> vertices)
+{
+    this->vertices = vertices;
+    if (!VBO || !VAO || !EBO) {
+        return Initialize();
+    };
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    void* ptr = glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
+    if (ptr) {
+        memcpy(ptr, vertices.data(), vertices.size() * sizeof(float));
+        glUnmapBuffer(GL_ARRAY_BUFFER);
+    }
+}
+
+RenderInstance::RenderInstance()
+	: VBO(0), VAO(0), EBO(0)
+{
+}
+
+RenderInstance::RenderInstance(unsigned int VBO, unsigned int VAO, unsigned int EBO)
+	: VBO(VBO), VAO(VAO), EBO(EBO)
+{
+}
+
+void RenderInstance::SetVBO(unsigned int& VBO)
+{
+    this->VBO = VBO;
+}
+
+void RenderInstance::SetVAO(unsigned int& VAO)
+{
+    this->VAO = VAO;
+}
+
+void RenderInstance::SetEBO(unsigned int& EBO)
+{
+    this->EBO = EBO;
+}
+
+const unsigned int& RenderInstance::GetVBO()
+{
+    return VBO;
+}
+
+const unsigned int& RenderInstance::GetVAO()
+{
+    return VAO;
+}
+
+const unsigned int& RenderInstance::GetEBO()
+{
+    return EBO;
+}
+
+const std::vector<float>& RenderInstance::GetVertices()
+{
+    return vertices;
+}
+
+void RenderInstance::SetVertices(std::vector<float> vertices)
+{
+    this->vertices = vertices;
+}
+
+bool RenderInstance::operator==(RenderInstance& other)
+{
+    return VAO = other.VAO && VBO == other.VBO && EBO == other.EBO && 
+        vertices == other.vertices;
+}
--- Dangeon&Dragons/Dodge/render/RenderInstance.h
+++ Dangeon&Dragons/Dodge/render/RenderInstance.h
@@ -1,12 +1,35 @@
 #pragma once
-struct RenderInstance
-{
-	unsigned int VBO, VAO, EBO;
+#include <vector>
 
+class RenderInstance
+{
+protected:
+	unsigned int VBO, VAO, EBO;
+	std::vector<float> vertices;
+private:
+	virtual void GenerateVertices() = 0;
+public:
 	RenderInstance();
 	RenderInstance(unsigned int VBO, unsigned int VAO, unsigned int EBO);
+	~RenderInstance() = default;
 
-	int GetId();
+	virtual void Initialize() = 0;
 
-	bool operator == (RenderInstance other);
+	void UpdateVertices();
+	void UpdateVertices(std::vector<float> vertices);
+
+	void SetVBO(unsigned int& VBO);
+	void SetVAO(unsigned int& VAO);
+	void SetEBO(unsigned int& EBO);
+
+	const unsigned int& GetVBO();
+	const unsigned int& GetVAO();
+	const unsigned int& GetEBO();
+
+	const std::vector<float>& GetVertices();
+	void SetVertices(std::vector<float> vertices);
+
+	virtual void Render() = 0;
+
+	bool operator == (RenderInstance& other);
 };
